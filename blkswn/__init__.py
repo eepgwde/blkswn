c87938000316cef63199f267f3a113b3630cde05
Walter Eaves <Napoleon.1er.Bonaparte@gmail.com>
2019-04-05 12:57:07 +0100

    -

---
+++ blkswn/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from blkswn._version import __version__
+from blkswn._Stack import Stack
+from blkswn._Queue import Queue
+
+__copyright__ = 'Copyright 2016 Walter Eaves'
+__license__ = 'GPLv3'
+__title__ = 'blkswn'
+
+# appease flake8: the imports are purposeful
+(__version__, Stack, Queue)
